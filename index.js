bd66da05b2fef7ea54c30ae17e3aeeaa06a93b34
afeiship <1290657123@qq.com>
2019-05-20 14:07:24 +0800

    fix: add fallback slution

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.warn('You can instead of this file to server url.')
+console.warn('You can instead of this file to server url.');
 console.warn('DO NOT DELETE THIS FILE!!!');
+
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+  for (const registration of registrations) {
+    registration.unregister();
+  }
+});
