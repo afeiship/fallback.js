1ff8fc1c3cca318efa93acf7f59a76b250dbd60a
afeiship <1290657123@qq.com>
2019-06-02 10:11:30 +0800

    fix: commente

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 console.warn('You can instead of this file to server url.');
 console.warn('DO NOT DELETE THIS FILE!!!');
 
+/*
+
 navigator.serviceWorker.getRegistrations().then(function(registrations) {
   for (const registration of registrations) {
     registration.unregister();
   }
 });
+
+*/
