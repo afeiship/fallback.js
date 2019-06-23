c64c8f0defb86436a2c2409ca3b056933b9cd9d6
afeiship <1290657123@qq.com>
2019-06-23 11:26:54 +0800

    add: sw_fallback

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 console.warn('You can instead of this file to server url.');
 console.warn('DO NOT DELETE THIS FILE!!!');
 
-/*
-
 if (navigator && navigator.serviceWorker) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for (const registration of registrations) {
-      registration.unregister();
-    }
-  });
+  if (window.__SW_DISABLED__) {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      for (const registration of registrations) {
+        registration.unregister();
+      }
+    });
+  }
 }
-
-*/
